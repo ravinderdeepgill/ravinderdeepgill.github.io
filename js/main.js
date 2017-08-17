1de2a3fd1f825f4340a5577307a7c637d8b03a2b
ravinderdeepgill <ravinderdeep.gill@gmail.com>
2017-08-17 16:40:26 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,11 +2,17 @@
 console.log('Hello from JS!');
 
 
+
+// $('a').on('click',function(event) {
+//   event.preventDefault();
+// });
+
 //When the user clicks the hambuger icon with a class of .hamburger
 //display Home, About, Projects, and Contects as a drop down
 $('.hamburger').on('click', function() {
   $('nav ul').toggleClass('show');
 });
+
 
 //When the user clicks the angle down icon, take it to the about me section with
 // a class of fa-angle-down with some transitions
@@ -15,10 +21,37 @@ $('.fa-angle-down').on('click', function() {
 });
 
 
+// Smooth scroll this mother!
+
+// Do it when someone clicks a nav link
+$('.smoothScroll').on('click', function(e) {
+  // prevent the standard link operation on click
+  e.preventDefault();
+  // use the href of the link to identify what
+  // section to scroll to
+  var thisTarget = $(this).attr('href');
+  // get that section's top offset
+  var targetOffset = $(thisTarget).offset().top - 60;
+  // use jQuery.animate() to animate the body's
+  // scrollTop to the targetOffest
+  $('body').animate({
+    scrollTop: targetOffset
+  }, 600);
+});
+
+// jQuery(document).ready(function($) {
+//
+// 	$(".scroll").click(function(event){
+// 		event.preventDefault();
+// 		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+// 	});
+// });
+
+$('.project').hide();
 
 //when the user clicks on the projects button with a class of "accordion"
 //display the div with a class of "project" by adding a class of showp n css
-$(".plusminus").text('-');
+$(".plusminus").text('+');
 $('button.accordion').on('click', function() {
   // $('.project').addClass('showp');
 
@@ -29,11 +62,11 @@ $('button.accordion').on('click', function() {
     $(".plusminus").text('+');
   }
   if ($(this).next('.project').is(':visible')) {
-    $(this).next('.project').slideUp('300');
+    $(this).next('.project').fadeOut('slow');
     $(this).children(".plusminus").text('+');
 
   } else {
-    $(this).next('.project').slideDown(300);
+    $(this).next('.project').fadeIn('slow');
     $(this).children(".plusminus").text('-');
   }
 });
